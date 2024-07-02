e966fce5c8bdb39ed37b2f07ca03aadea4f64882
elenocastro <elenohernan@hotmail.com>
2024-07-02 00:11:16 +0000

    new bars and tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,17 @@ egra_url = 'https://www.dropbox.com/scl/fi/pswt5c75c2o2v0csix4mr/EGRA.csv?rlkey=
 docentes_url = 'https://www.dropbox.com/scl/fi/ub2g0606rmqu4ykn4ef15/Docentes.csv?rlkey=3v26fp1cp4tjam3j7si17f5e2&st=aea42d23&dl=1'
 docentes_auto_url = 'https://www.dropbox.com/scl/fi/o7fhl9bvp1ey89qdwworu/Docentes-Autoadministrada.csv?rlkey=0a8a8gg61eus8bssvilievbkk&st=86wyxizg&dl=1'
 videos_url = 'https://www.dropbox.com/scl/fi/odckndu43clmh948gohfa/videos_teach.xlsx?rlkey=v48p8nw6da2eo36vjg0xntsjq&st=083x6e8q&dl=1'
+docentes_ce_url = 'https://www.dropbox.com/scl/fi/p27k5o7zup7igecfxrjkk/CONTINUIDAD_27062024.xlsx?rlkey=j52pkyf338d1d0ym76igi0440&st=xouj06hu&dl=1'
+assignment_ce_url = 'https://www.dropbox.com/scl/fi/uvh67un6k4e6en2yccr7d/assignment_groups_03072023.dta?rlkey=ck70b2ybt7a6hiccpfz5umgwx&st=uud5uifp&dl=1'
 
 # Leer el archivo CSV desde Dropbox
 egra = pd.read_csv(egra_url)
 docentes = pd.read_csv(docentes_url)
 docentes_auto = pd.read_csv(docentes_auto_url)
 videos = pd.read_excel(videos_url)
+docentes_ce = pd.read_excel(docentes_ce_url)
+assignment_ce = pd.read_stata(assignment_ce_url)
+
 
 # Convertir columnas de fechas a tipo datetime
 egra['SubmissionDate'] = pd.to_datetime(egra['SubmissionDate'])
@@ -50,18 +55,13 @@ encuestas_por_fecha_total.columns = ['EGRA', 'Docentes', 'Docentes-Autoadministr
 encuestas_por_fecha_total = encuestas_por_fecha_total.fillna(int(0))
 encuestas_por_fecha_total = encuestas_por_fecha_total.sort_index(ascending=False)
 
-# Configurar el dashboard
-st.header('Número de encuestas subidas por fecha de inicio')
+
 # Convertir el índice a string para que solo muestre la fecha sin la hora
 # encuestas_por_fecha_total.index = encuestas_por_fecha_total.index.astype(str)
 encuestas_por_fecha1.index = encuestas_por_fecha1.index.astype(str)
 encuestas_por_fecha2.index = encuestas_por_fecha2.index.astype(str)
 encuestas_por_fecha3.index = encuestas_por_fecha3.index.astype(str)
 encuestas_por_fecha4.index = encuestas_por_fecha4.index.astype(str)
-
-
-# Mostrar el conteo de encuestas por fecha en una tabla
-st.dataframe(encuestas_por_fecha_total.style.format("{:.0f}"))
 
 # Calcular los totales para las barras de progreso
 totals = encuestas_por_fecha_total.sum(axis=0)
@@ -79,34 +79,126 @@ progreso_egra = int(egras_total / meta_egra * 100)
 progreso_docentes = int(docentes_total / meta_docentes * 100)
 progreso_videos = int(videos_total / meta_videos * 100)
 
-# Mostrar las barras de progreso con etiquetas de porcentaje
-st.header('Progreso hacia las metas')
 
-st.subheader('EGRA')
-st.progress(progreso_egra)
-st.write(f'Progreso: {progreso_egra}%')
 
-st.subheader('Docentes (incluye autoadministradas)')
-st.progress(progreso_docentes)
-st.write(f'Progreso: {progreso_docentes}%')
 
-st.subheader('Videos Teach')
-st.progress(progreso_videos)
-st.write(f'Progreso: {progreso_videos}%')
 
-# Crear un gráfico de barras para visualizar los datos
-st.header('Frecuencia diaria')
-st.subheader('EGRA')
-st.bar_chart(encuestas_por_fecha1)
+tab1, tab2, tab3 = st.tabs(["Datos Totales por Fecha", "Datos Totales por Centro Educativo", 
+                      'Datos recolectados por Grupo de Tratamiento'])
 
-# Crear un gráfico de barras para visualizar los datos
-st.subheader('Docentes')
-st.bar_chart(encuestas_por_fecha2)
+with tab1:
+    # Mostrar las barras de progreso con etiquetas de porcentaje
+    st.header('Progreso hacia las metas')
 
-# Crear un gráfico de barras para visualizar los datos
-st.subheader('Docentes - Autoadministrada')
-st.bar_chart(encuestas_por_fecha3)
+    st.write(f'EGRA. Progreso: {progreso_egra}% - ({int(egras_total)}/{meta_egra})')
+    st.progress(progreso_egra)
+    #st.write(f'Progreso: {progreso_egra}% - ({int(egras_total)}/{meta_egra})')
 
-# Crear un gráfico de barras para visualizar los datos
-st.subheader('Videos')
-st.bar_chart(encuestas_por_fecha4)
+    st.write(f'Docentes. Progreso: {progreso_docentes}% - ({int(docentes_total)}/{meta_docentes})')
+    st.progress(progreso_docentes)
+    #st.write(f'Progreso: {progreso_docentes}% - ({int(docentes_total)}/{meta_docentes})')
+
+    st.write(f'Videos Teach. Progreso: {progreso_videos}% - ({int(videos_total)}/{meta_videos})')
+    st.progress(progreso_videos)
+    #st.write(f'Progreso: {progreso_videos}% - ({int(videos_total)}/{meta_videos})')
+
+    # Configurar el dashboard
+    st.header('Número de encuestas subidas por fecha de inicio')
+    # Mostrar el conteo de encuestas por fecha en una tabla
+    st.dataframe(encuestas_por_fecha_total.style.format("{:.0f}"))
+
+    # Crear un gráfico de barras para visualizar los datos
+    st.header('Frecuencia diaria')
+    st.subheader('EGRA')
+    st.bar_chart(encuestas_por_fecha1)
+
+    # Crear un gráfico de barras para visualizar los datos
+    st.subheader('Docentes')
+    st.bar_chart(encuestas_por_fecha2)
+
+    # Crear un gráfico de barras para visualizar los datos
+    st.subheader('Docentes - Autoadministrada')
+    st.bar_chart(encuestas_por_fecha3)
+
+    # Crear un gráfico de barras para visualizar los datos
+    st.subheader('Videos')
+    st.bar_chart(encuestas_por_fecha4)
+
+
+## Total de datos a nivel de centro educativo
+
+docentes_auto = docentes_auto.merge(docentes_ce[['Código', 'unique_id']], left_on = 'docente', right_on = 'unique_id', how = 'left')
+docentes = docentes.merge(docentes_ce[['Código', 'unique_id']], left_on = 'docente', right_on = 'unique_id', how = 'left')
+
+#contando encuetas a nivel de centro educativo
+egra_x_ce = egra['School'].value_counts()
+docentes_x_ce = docentes['Código'].value_counts()
+docentes_auto_x_ce = docentes_auto['Código'].value_counts()
+videos_ce = videos[videos['Name Valid']]['CE Code'].value_counts()
+
+encuestas_ce = pd.concat([egra_x_ce, docentes_x_ce, docentes_auto_x_ce, videos_ce], axis = 1)
+encuestas_ce.columns = ['E', 'D', 'DA', 'V'] 
+encuestas_ce = encuestas_ce.fillna(0)
+ce = docentes_ce[['Código', 'Centroeducativo', 'Latitud', 'Longitud']].drop_duplicates()
+
+#asignando geo
+encuestas_ce = ce.merge(encuestas_ce, right_index = True, left_on = ['Código'], how = 'right')
+#asignando tratamiento
+encuestas_ce = encuestas_ce.merge(assignment_ce[['treatment', 'Código']], on = 'Código', how= 'left')
+encuestas_ce.set_index(['Código', 'Centroeducativo'], inplace = True)
+
+with tab2:
+    st.header('Número de encuestas por Centro Educativo')
+    st.write('E - EGRA')
+    st.write('D - Docentes')
+    st.write('DA - Docentes - Autoadministrados')
+    st.write('V - Videos')
+    
+    # Añadir buscador para filtrar por centro educativo
+    search_term = st.text_input('Buscar por Centro Educativo')
+    if search_term:
+        filtered_data = encuestas_ce[encuestas_ce.index.get_level_values('Centroeducativo').str.contains(search_term, case=False)]
+    else:
+        filtered_data = encuestas_ce
+    
+    # Convertir los valores a enteros
+    filtered_data = filtered_data[['E', 'D', 'DA', 'V']].astype(int)
+    st.dataframe(filtered_data)
+
+    # Mostrar el mapa
+    st.header('Mapa de Centros Educativos Encuestados')
+
+    mapa_data = encuestas_ce.loc[encuestas_ce.Latitud != 0, ['Latitud', 'Longitud']]
+    mapa_data.columns = ['lat', 'lon']
+    st.map(mapa_data)
+
+with tab3:
+    # Metas
+    metas = {
+        "Control": 274,
+        "Training only": 293,
+        "Training+FUSAL": 298,
+        "Training+GP": 277,
+        "Training+Nudges": 266
+    }
+
+    encuestas_ce['DT'] = encuestas_ce[['D','DA']].sum(axis = 1)
+    encuestas_tratamiento = encuestas_ce.groupby(['treatment'])[['E','DT','V']].sum()
+    # st.table(encuestas_tratamiento.astype(int))
+
+    for grupo, row in encuestas_tratamiento.iterrows():
+        meta = metas[grupo]
+        
+        progreso_egra = int((row['E'] / 1000) * 100)
+        progreso_docentes = int((row['DT'] / meta) * 100)
+        progreso_videos = int((row['V'] / meta) * 100)
+        
+        st.subheader(grupo)
+        st.text(f"EGRA: {progreso_egra}%")
+        st.progress(progreso_egra)
+        
+        st.text(f"Docentes: {progreso_docentes}%")
+        st.progress(progreso_docentes)
+        
+        st.text(f"Videos: {progreso_videos}%")
+        st.progress(progreso_videos)
